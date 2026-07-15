7dd9dbd268b1dc9fd7e85a4e3a317a86aae8e4af
Ivan <ivangegov.dve@gmail.com>
2026-07-15 09:15:01 +0300

    test(open-overview): reuse isolated preview listener

--- scratch/tests/open-overview.playwright.config.js
+++ scratch/tests/open-overview.playwright.config.js
@@ -14,7 +14,7 @@ module.exports = defineConfig({
   fullyParallel: false,
   workers: 1,
   use: { baseURL: "http://127.0.0.1:4174", trace: "retain-on-failure" },
-  webServer: { command: "node open-overview-static-server.mjs ../../vercel-public 4174", cwd: __dirname, url: "http://127.0.0.1:4174/web/open-overview/index.html", reuseExistingServer: false, timeout: 15_000 },
+  webServer: { command: "node open-overview-static-server.mjs ../../vercel-public 4174", cwd: __dirname, url: "http://127.0.0.1:4174/web/open-overview/index.html", reuseExistingServer: true, timeout: 15_000 },
   projects: [{
     name: "chromium",
     use: {
